ad02cfc6bf01ff89c45313e6ac7dc2e2fef99060
Jorge Díaz <jorgedf@gmail.com>
2015-11-26 01:39:58 +0100

    Add NDC message validation logic using Schemas.

--- api/api_ndc_endpoint.rb
+++ api/api_ndc_endpoint.rb
@@ -1,12 +1,18 @@
 require 'grape'
 require 'nokogiri'
 
+SCHEMAS_DIR = "./api/schemas/"
+SCHEMAS_VERSION = 'v113-p15-2'
+
 module API
 
-  class XMLMessage < Grape::Validations::Base
+  module Errors
+    class IvalidNDCMessageError < RuntimeError; end
+    class IvalidNDCValidationError < RuntimeError; end
+    class IvalidNDCFormatError < RuntimeError; end
   end
 
-  NDC_METHODS = [:AirShopping, :FlightPrice, :SeatAvailability, :ServiceList, :ServicePrice]
+  NDC_METHODS = [:AirShoppingRQ, :FlightPriceRQ, :SeatAvailabilityRQ, :ServiceListRQ, :ServicePriceRQ]
 
   class NDCEndpoint < Grape::API
 
@@ -18,6 +24,19 @@ module API
         error!("Malformed Request :: XML structure format invalid", 400)
       elsif !NDC_METHODS.include?(@ndc_method = @doc.root.name.to_sym)
         error!("Malformed Request :: Invalid or unrecognized NDC method (#{@ndc_method})", 400)
+      else
+        errors = []
+        schemas_path = SCHEMAS_DIR + SCHEMAS_VERSION
+        Dir.chdir(schemas_path) do
+          begin
+            schema_file = "#{@ndc_method}.xsd"
+            xsd = Nokogiri::XML::Schema(File.read(schema_file))
+            errors += xsd.validate(@doc)
+          rescue => e
+            errors << Errors::IvalidNDCValidationError.new("NDC Validation Error :: #{e.message}")
+          end
+          error!("Malformed NDC message :: #{errors.join(' | ')}", 400) unless errors.empty?
+        end
       end
     end
 
