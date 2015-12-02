2de31e89a0d5d46a04cc12df0dcc28b76aa38729
Jorge Díaz <jorgedf@gmail.com>
2015-12-02 02:34:47 +0100

    Minor changes in rack init process.

--- init.rb
+++ init.rb
@@ -1,19 +1,24 @@
 require 'pry' if $RACK_ENV == 'development'
 
+require 'active_record'
 class ::Logger; alias_method :write, :<<; end # for Rack::CommonLogger
 
 # $LOG_FILE = "./log/#{$RACK_ENV}.log"
 puts "Initializing logfile in: #{STDOUT}"
 $logger = ::Logger.new(STDOUT)
 
-puts "Connecting DB for environment: #{$RACK_ENV}..."
+
+puts "Loading DB config for environment: #{$RACK_ENV}..."
 if $RACK_ENV == 'production'
   @config = ENV["DATABASE_URL"]
 else
   @config = YAML.load_file('config/database.yml')[$RACK_ENV]
 end
+puts "Config loaded: #{@config.inspect}"
 
 if @config.present?
+  puts "Connecting DB for environment: #{$RACK_ENV}..."
+  ActiveRecord::Base.clear_active_connections!
   ActiveRecord::Base.establish_connection @config
   ActiveRecord::Base.logger = $logger
 end
