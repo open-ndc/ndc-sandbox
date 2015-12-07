967c401b114ac2a03f6ce357e12edbfe5d285f73
Jorge Díaz <jorgedf@gmail.com>
2015-12-07 03:38:02 +0100

    AirShopping message refactored to use Offer model.

--- api/messages/base.rb
+++ api/messages/base.rb
@@ -4,19 +4,17 @@ module API
 
     class Base
 
+      attr_reader :timestamp, :token, :version, :namespaces, :name, :owner
+
       def initialize(doc)
+        @name = 'OpenNDC Sandbox'
+        @version = '1.1.3'
+        @owner = 'FA'
         @timestamp = Time.now.utc.iso8601
         @token = Digest::SHA1.hexdigest @timestamp
-        @version = '1.1.5'
-        @transaction_identifier = 'TR-00000'
-        binding.pry
         @namespaces = {
           'xmlns' => "http://www.iata.org/IATA/EDIST",
           'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
-          'EchoToken' => @token,
-          'TimeStamp' => @timestamp,
-          'Version' => @version,
-          'TransactionIdentifier' => @transaction_identifier
         }
       end
 
