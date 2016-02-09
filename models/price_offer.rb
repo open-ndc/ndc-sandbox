4716fb942cc5c78870edca2d8dc2374d9781dad1
IgorPetuh <i.petuh@icloud.com>
2016-02-09 14:44:12 +0300

    Fixes usage of Time tag

--- models/price_offer.rb
+++ models/price_offer.rb
@@ -14,34 +14,41 @@ class PriceOffer < BaseOffer
     originDestinations.each_with_index do |flights, index|
       orig = flights.first['Departure']['AirportCode']
       dest = flights.last['Arrival']['AirportCode']
-      break unless (route = Route.joins(:airline, :fares).where(origin: orig, destination: dest, departure_time: flights.first['Departure']['Time']).first)
-      offer_flight_segments = []
-      flights.each do |flight|
-        result = FlightSegment.joins(:route)
-                    .where(
-                        departure_airport_code: flight['Departure']['AirportCode'],
-                        arrival_airport_code: flight['Arrival']['AirportCode'],
-                        departure_time: flight['Departure']['Time'],
-                        arrival_time: flight['Arrival']['Time'],
-                        routes: {
-                            origin: orig,
-                            destination: dest,
-                        }
-                    )
-                    .where('flight_segments.departure_mask & ? > 0', FlightSegment.available_mask([flight['Departure']['Date']]))
-                    .where('flight_segments.arrival_mask & ? > 0', FlightSegment.available_mask([flight['Arrival']['Date']]))
-        break unless (fs = result.first)
-        new_segment = DataList::ListItem.new(fs.id, {ref_key: 'SEG', data: fs})
-        offer_flight_segments << new_segment
-        _new_key = full_flight_segments_list << new_segment
-      end
-      route.fares.each do |fare|
-        offers << PriceOffer.new(
-            dest_key: OD_KEY + (index + 1).to_s,
-            fare: fare,
-            flight_segments: offer_flight_segments,
-            passengers_keys: [],
-        )
+      routes = Route.joins(:airline, :fares).where(origin: orig, destination: dest)
+      routes.each do |route|
+        offer_flight_segments = []
+        flights.each do |flight|
+          add_params = {
+              departure_time: flight['Departure']['Time'],
+              arrival_time: flight['Arrival']['Time'],
+          }.select!{|_key, value| value.present?}
+
+          result = FlightSegment.joins(:route)
+                      .where(add_params)
+                      .where(
+                          departure_airport_code: flight['Departure']['AirportCode'],
+                          arrival_airport_code: flight['Arrival']['AirportCode'],
+                          routes: {
+                              id: route.id
+                          }
+                      )
+                      .where('flight_segments.departure_mask & ? > 0', FlightSegment.available_mask([flight['Departure']['Date']]))
+          if flight['Arrival']['Date']
+            result.where('flight_segments.arrival_mask & ? > 0', FlightSegment.available_mask([flight['Arrival']['Date']]))
+          end
+          break unless (fs = result.first)
+          new_segment = DataList::ListItem.new(fs.id, {ref_key: 'SEG', data: fs})
+          offer_flight_segments << new_segment
+          _new_key = full_flight_segments_list << new_segment
+        end
+        route.fares.each do |fare|
+          offers << PriceOffer.new(
+              dest_key: OD_KEY + (index + 1).to_s,
+              fare: fare,
+              flight_segments: offer_flight_segments,
+              passengers_keys: [],
+          )
+        end
       end
     end
     {offers: offers, full_flight_segments_list: full_flight_segments_list}
