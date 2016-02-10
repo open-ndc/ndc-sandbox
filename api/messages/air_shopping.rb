a3ec3fd68118998e65a06fbf78b1a80e69da162b
zaali kavelashvili <zaali@live.com>
2016-02-10 22:21:05 +0400

    User pure date string instead of DateTime type

--- api/messages/air_shopping.rb
+++ api/messages/air_shopping.rb
@@ -21,7 +21,7 @@ module API
         date_dep = DateTime.parse(ond.xpath('Departure/Date').text) if ond.xpath('Departure/Date')
         date_arr = DateTime.parse(ond.xpath('Arrival/Date').text) if ond.xpath('Arrival/Date').present?
         num_travelers = @doc.xpath('/AirShoppingRQ/Travelers/Traveler/AnonymousTraveler/PTC').first.attributes["Quantity"].value ? doc.xpath('/AirShoppingRQ/Travelers/Traveler/AnonymousTraveler/PTC').first.attributes["Quantity"].value.to_i : nil
-        save_request(dep, arr, date_dep)
+        save_request(dep, arr, ond.xpath('Departure/Date').text)
         results = Offer.fetch_by_ond_and_dates(dep, arr, date_dep, date_arr, num_travelers)
         @offers = results[:offers]
         @datalist_flight_segments = results[:datalists][:flight_segments]
