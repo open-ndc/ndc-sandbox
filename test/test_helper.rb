c12e3a36a2cb0a3c386b21191bc0a8f22c2d7426
zaali kavelashvili <zaali@live.com>
2016-02-15 21:24:52 +0400

    Remove jruby option from unit testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,3 @@ require 'test/unit'
 
 require 'net-http-spy'
 Net::HTTP.http_logger_options = {trace: true, verbose: false}
-  
-case RUBY_ENGINE
-when 'ruby'
-  require 'pry-byebug'
-when 'jruby'
-  require 'pry'
-end
