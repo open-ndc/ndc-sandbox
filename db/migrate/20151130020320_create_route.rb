a750e8c2807d63470e92ce8726c7283927d8d455
zaali kavelashvili <zaali@live.com>
2016-02-19 13:40:12 +0400

    Remove departure_time column from Route

--- db/migrate/20151130020320_create_route.rb
+++ db/migrate/20151130020320_create_route.rb
@@ -5,7 +5,6 @@ class CreateRoute < ActiveRecord::Migration
       t.references :airline
       t.string :origin, limit: 3, index: true
       t.string :destination, limit: 3, index: true
-      t.string :departure_time, limit: 5
     end
   end
 
