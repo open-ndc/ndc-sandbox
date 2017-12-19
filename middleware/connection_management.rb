68ea1591bc8e8033897a8eafbbe0b47f961923bc
Óscar de Arriba <odarriba@gmail.com>
2017-12-19 14:22:47 +0100

    Add removed middleware from Rails 4

---
+++ middleware/connection_management.rb
@@ -0,0 +1,25 @@
+require 'active_record'
+
+module Middleware
+
+  class ConnectionManagement
+
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      testing = env['rack.test']
+
+      status, headers, body = @app.call(env)
+      proxy = ::Rack::BodyProxy.new(body) do
+        ActiveRecord::Base.clear_active_connections! unless testing
+      end
+      [status, headers, proxy]
+    rescue Exception
+      ActiveRecord::Base.clear_active_connections! unless testing
+      raise
+    end
+
+  end
+end
